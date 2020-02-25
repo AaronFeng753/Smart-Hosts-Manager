17a889ef7c180bbd07428e1db1f6749ce3ddf5bf
AaronFeng753 <52553688+AaronFeng753@users.noreply.github.com>
2020-02-25 15:39:19 +0800

    Add files via upload

---
+++ SRC/Smart-Hosts-Manager/mainwindow.cpp
@@ -0,0 +1,153 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    Init_Table();
+    connect(this, SIGNAL(Send_TestAndOutput_Fin()), this, SLOT(TestAndOutput_Fin()));
+    connect(this, SIGNAL(Send_TextBrowser_NewMSG(QString)), this, SLOT(TextBrowser_NewMSG(QString)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::Init_Table()
+{
+    Table_model->setColumnCount(2);
+    Table_model->setHeaderData(0, Qt::Horizontal, "网址");
+    Table_model->setHeaderData(1, Qt::Horizontal, "IP");
+    ui->tableView->horizontalHeader()->setDefaultAlignment(Qt::AlignLeft);
+    ui->tableView->setModel(Table_model);
+    //=============================================
+    //将横向表头设置为可见
+    ui->tableView->horizontalHeader()->setVisible(1);
+}
+
+void MainWindow::on_pushButton_AddWebSite_clicked()
+{
+    ui->tableView->setUpdatesEnabled(false);
+    int rowNum = Table_model->rowCount();
+    Table_model->setItem(rowNum, 0, new QStandardItem(ui->lineEdit_addWebSite->text()));
+    //========
+    QAbstractItemModel *modessl = Table_model;
+    QModelIndex indextemp = modessl->index(rowNum, 1);
+    ui->tableView->scrollTo(indextemp);
+    //========
+    ui->tableView->setUpdatesEnabled(true);
+}
+
+void MainWindow::on_pushButton_AddIP_clicked()
+{
+    if(curRow!=-1)
+    {
+        ui->tableView->setUpdatesEnabled(false);
+        int rowNum = curRow;
+        Table_model->setItem(rowNum, 1, new QStandardItem(ui->lineEdit_addIP->text()));
+        //========
+        QAbstractItemModel *modessl = Table_model;
+        QModelIndex indextemp = modessl->index(rowNum, 1);
+        ui->tableView->scrollTo(indextemp);
+        //========
+        ui->tableView->setUpdatesEnabled(true);
+    }
+}
+
+void MainWindow::on_tableView_clicked(const QModelIndex &index)
+{
+    int curRow_new = ui->tableView->currentIndex().row();
+    if(curRow_new == curRow)
+    {
+        curRow = -1;
+        ui->tableView->clearSelection();
+    }
+    else
+    {
+        curRow = curRow_new;
+    }
+}
+
+void MainWindow::on_pushButton_TestAndOutput_clicked()
+{
+    ui->pushButton_TestAndOutput->setEnabled(0);
+    OutPut_List.clear();
+    ui->textBrowser_progress->append("开始测试选优, 请耐心等待.");
+    QtConcurrent::run(this, &MainWindow::TestAndOutput);
+}
+void MainWindow::TestAndOutput()
+{
+    int MAXrowNum = Table_model->rowCount();
+    QAbstractItemModel *modessl = Table_model;
+    for(int rowNum=0; rowNum<MAXrowNum; rowNum++)
+    {
+        BestIP="";
+        QModelIndex indextemp = modessl->index(rowNum, 0);
+        QVariant datatemp = modessl->data(indextemp);
+        QString WebSite = datatemp.toString();
+        //===============
+        indextemp = modessl->index(rowNum, 1);
+        datatemp = modessl->data(indextemp);
+        QString IP_str = datatemp.toString();
+        QStringList IP_List = IP_str.split(",");
+        IP_List.removeAll("");
+        //===============
+        QtConcurrent::run(this, &MainWindow::Ping,IP_List);
+        while (BestIP=="")
+        {
+            QThread::msleep(500);
+        }
+        OutPut_List.append(BestIP+" "+WebSite+"\n");
+    }
+    emit Send_TestAndOutput_Fin();
+}
+
+void MainWindow::TestAndOutput_Fin()
+{
+    for(int i=0; i<OutPut_List.size(); i++)
+    {
+        ui->textBrowser_hosts->append(OutPut_List.at(i));
+    }
+    ui->pushButton_TestAndOutput->setEnabled(1);
+}
+
+void MainWindow::Ping(QStringList IP_List)
+{
+    QString BestIP_tmp = "";
+    qint64 MinTimeCost = 9999999999;
+    qint64 timeCost;
+    for(int i=0; i<IP_List.size(); i++)
+    {
+        timeCost =0 ;
+        QString IP = IP_List.at(i);
+        IP = IP.trimmed();
+        QProcess Ping_cmd;
+        qint64 StartTime = QDateTime::currentMSecsSinceEpoch();
+        Ping_cmd.start("ping "+IP);
+        Ping_cmd.waitForStarted(10000);
+        Ping_cmd.waitForFinished(10000);
+        qint64 EndTime = QDateTime::currentMSecsSinceEpoch();
+        timeCost = EndTime-StartTime;
+        emit Send_TextBrowser_NewMSG("IP:["+IP+"]  耗时:["+QString::number(timeCost,10)+"]");
+        if(timeCost<=MinTimeCost)
+        {
+            MinTimeCost= timeCost;
+            BestIP_tmp = IP;
+        }
+    }
+    BestIP = BestIP_tmp;
+}
+
+void MainWindow::TextBrowser_NewMSG(QString msg)
+{
+    ui->textBrowser_progress->append(msg);
+}
+
+void MainWindow::on_pushButton_CopyHosts_clicked()
+{
+    QClipboard *clip=QApplication::clipboard();
+    clip->setText(ui->textBrowser_hosts->toPlainText());
+}
---
+++ SRC/Smart-Hosts-Manager/mainwindow.h
@@ -0,0 +1,60 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <QStandardItemModel>
+#include <QtConcurrent>
+#include <QProcess>
+#include <QTime>
+#include <QClipboard>
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    //========================
+    void Init_Table();
+    int curRow=-1;
+    QStandardItemModel *Table_model = new QStandardItemModel();
+    QString BestIP ="";
+    void Ping(QStringList IP_List);
+    void TestAndOutput();
+    QStringList OutPut_List;
+
+    //========================
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_AddWebSite_clicked();
+
+    void on_pushButton_AddIP_clicked();
+
+    void on_tableView_clicked(const QModelIndex &index);
+
+    void on_pushButton_TestAndOutput_clicked();
+
+    void on_pushButton_CopyHosts_clicked();
+
+signals:
+    void Send_TestAndOutput_Fin();
+    void Send_TextBrowser_NewMSG(QString msg);
+
+public slots:
+    void TestAndOutput_Fin();
+    void TextBrowser_NewMSG(QString msg);
+
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
